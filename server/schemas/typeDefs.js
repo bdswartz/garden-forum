7a925703fccb3fea738e413229f6e8004e3bd2d0
david curtis <heythereimdavidcurtis@gmail.com>
2022-06-01 11:07:16 -0400

    added a few typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
   type User {
     _id: ID
+    createdAt: String
     firstName: String
     lastName: String
+    username: String
     email: String
+    posts: [Post]
+    plants: [Plant]
+    friends: [User]
+  }
+
+  type Plant {
+    scientific_name: String
+    common_name: String
+    image_path: String
+    usda_zone: String
+    pruning: String
+    fertilization: String
+    water: String
+    plantHistory: [PlantHistory]
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
+  type PlantHistory {
+    createdAt: String
+    note_body: String
   }
 
   type Auth {
@@ -15,13 +51,37 @@ const typeDefs = gql`
   }
 
   type Query {
-    user: User
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+    plant(_id: ID!): Plant
+    plants(username: String): [Plant]
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
     login(email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addComment(postId: ID!, commentBody: String!): Post
+    addPlant()
+    addPlantHistory()
+    updatePlant()
+    removePlantHistory()
+    removePlant()
+
   }
 `;
 
