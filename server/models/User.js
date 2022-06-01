b6c206a06f26e51b2cd16304baed11edb5b64240
david curtis <heythereimdavidcurtis@gmail.com>
2022-06-01 09:31:31 -0400

    added user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,33 +1,65 @@
 const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const postSchema = require('./Post');
+const plantSchema = require('./Plant');
 
-const { Schema } = mongoose;
+const { Schema, model } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
   firstName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   lastName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
-    minlength: 5
-  }
+    minlength: 5,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+  plants: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Plant',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -37,9 +69,13 @@ userSchema.pre('save', async function(next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 const User = mongoose.model('User', userSchema);
 
