8e8dbad84a7d1d089043628188fd55d0cc48ecd8
byron4455 <byronstephenson1@gmail.com>
2022-06-04 03:30:29 -0400

    tweaked textarea

--- client/src/components/ForumPosts/Search.js
+++ client/src/components/ForumPosts/Search.js
@@ -4,11 +4,24 @@ import Fab from '@mui/material/Fab';
 import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Igor from '../../assets/images/igor.jpg';
+
 
 
 
 export default function Search() {
     return (
+      <Box
+    className="image"
+    style={{
+    backgroundImage: `url(${Igor})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundSize: "cover",
+    // height: "100vh",
+    color: "#f5f5f5",
+    backgroundPosition: 'left',
+}}>
       <Grid container
       direction="row"
       justifyContent="flex-end"
@@ -18,15 +31,21 @@ export default function Search() {
         pb: 10,
         pr: 5,
       }}>
-          <TextField id="outlined-basic" label="Search.." variant="outlined" style = {{width: 500}} />
+          <TextField id="outlined-basic" label="Search.." 
+          variant="outlined" style = {{width: 500}} 
+          InputProps={{ style: { fontSize: 16 } }}
+        InputLabelProps={{ style: { fontSize: 16 } }}/>
           <Fab variant="extended"
           sx={{
             ml: 3,
+            mb: 1,
+            backgroundColor: '#4caf50'
           }}>
-        <SearchIcon sx={{ mr: 1 }} />
+        <SearchIcon sx={{ mr: 1, }} />
         Search
       </Fab>
       </Grid>
+      </Box>
     );
   }
 
