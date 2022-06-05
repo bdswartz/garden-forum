b46bf2e705da069e80fa98bf757587cdbdeff597
jambrose0 <jambrose0518@gmail.com>
2022-06-05 18:46:39 -0400

    plant dialog prefills relevant information from API request

--- client/src/components/AddPlantDialog/index.js
+++ client/src/components/AddPlantDialog/index.js
@@ -11,39 +11,83 @@ import {
   DialogTitle,
 } from "@mui/material";
 import { searchPlants } from "../../utils/API";
-import { useQuery } from "@apollo/client";
-import { QUERY_PLANT } from "../../utils/queries";
+import { useMutation } from "@apollo/client";
+import { QUERY_PLANT, ME } from "../../utils/queries";
+import { ADD_PLANT } from "../../utils/mutations";
 
 export default function AddPlantDialog({ open, handleClose }) {
   // const [toggle, setToggle] = useState(false)
   const plantFile = useRef();
   const [plantImg, setPlantImg] = useState([]);
+  // const [plantArr, setPlantArr] = useState([]);
+  const [plantName, setplantName] = useState();
+  const [sciencePlant, setSciencePlant] = useState();
+  const [plantPic, setPlantPic] = useState();
 
   const onFileChange = (event) => {
     setPlantImg(event.target.files);
   };
 
-  let plant_name = "";
-  let scientific_name = "";
-  let plant_img = "";
+  const [addPlant, { error }] = useMutation(ADD_PLANT, {
+    update(
+      cache,
+      {
+        data: {
+          commonName,
+          ScientificName,
+          imagePath,
+          pruning,
+          watering,
+          fertilization,
+        },
+      }
+    ) {
+      try {
+        const { me } = cache.readQuery({
+          query: ME,
+        });
+        cache.writeQuery({
+          query: ME,
+          data: { me: { ...me, plants: [...me.plants, addPlant] } },
+        });
+      } catch (e) {
+        console.warn("First plant by user!");
+      }
+    },
+  });
 
   const handleSearch = () => {
     const plantArr = Object.values(plantImg);
     searchPlants(plantArr).then((res) => {
-      plant_name = res.suggestions[0].plant_details.common_names[0];
-      scientific_name = res.suggestions[0].plant_details.scientific_name;
-      plant_img = res.suggestions[0].similar_images[0].url;
+      setplantName(res.suggestions[0].plant_details.common_names[0]);
+      setSciencePlant(res.suggestions[0].plant_details.scientific_name);
+      setPlantPic(res.suggestions[0].similar_images[0].url);
 
-      console.log(plant_name);
-      console.log(scientific_name);
-      console.log(plant_img);
-
-      // let plant_name = res.suggestions[0].plant_details.common_names[0];
-      // const scientific_name = res.suggestions[0].plant_details.scientific_name;
-      // const plant_img = res.suggestions[0].similar_images[0].url;
-
-      return plant_name;
+      // console.log(plant_name);
+      // console.log(res.suggestions[0].plant_details.scientific_name);
+      // console.log(res.suggestions[0].similar_images[0].url);
     });
+    console.log(plantName);
+    console.log(sciencePlant);
+    console.log(plantPic);
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addPlant({
+        variables: {
+          commonName,
+          ScientificName,
+          imagePath,
+          pruning,
+          watering,
+          fertilization,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -72,19 +116,24 @@ export default function AddPlantDialog({ open, handleClose }) {
             margin="dense"
             label="Common Plant Name"
             type="text"
+            // key={common_name}
             fullWidth
             variant="standard"
-            // value={plant_name}
+            value={plantName}
+            // onChange={(res) =>
+            // plantArr(res.suggestions[0].plant_details.common_names[0])
+            // }
           />
-          {plant_name}
           <TextField
             autoFocus
             margin="dense"
             id="name"
             label="Scientific Plant Name"
             type="text"
+            // key={scientific_name}
             fullWidth
             variant="standard"
+            value={sciencePlant}
           />
           <TextField
             autoFocus
@@ -116,7 +165,7 @@ export default function AddPlantDialog({ open, handleClose }) {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Add Plant!</Button>
+          <Button onSubmit={handleFormSubmit}>Add Plant!</Button>
         </DialogActions>
       </Dialog>
     </div>
