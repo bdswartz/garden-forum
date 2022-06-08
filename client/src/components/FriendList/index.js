fda05766da928742535d1191f995fbabb2353625
jambrose0 <jambrose0518@gmail.com>
2022-06-08 11:05:06 -0400

    reworking styling of plant page

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-import PersonIcon from '@mui/icons-material/Person';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
-import { green } from '@mui/material/colors';
+import React from "react";
+import { Link } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import PersonIcon from "@mui/icons-material/Person";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import { green } from "@mui/material/colors";
 
 const FriendList = ({ friendCount, username, friends }) => {
   if (!friends || !friends.length) {
     return (
       <>
         <Typography
-          variant='h6'
-          sx={{ p: 2, p: 1, bgcolor: green[500], color: 'white' }}
+          variant="h6"
+          sx={{ p: 2, p: 1, bgcolor: green[500], color: "white" }}
         >
           Friend List
         </Typography>
-        <Typography align='center' sx={{ height: 60, pt: 2 }}>
+        <Typography align="center" sx={{ height: 60, pt: 2 }}>
           {username} has 0 friends
         </Typography>
       </>
@@ -35,10 +35,14 @@ const FriendList = ({ friendCount, username, friends }) => {
   }
 
   return (
-    <div>
+    <Box
+      sx={{
+        justifyContent: "center",
+      }}
+    >
       <Typography
-        variant='h6'
-        sx={{ p: 2, p: 1, bgcolor: green[500], color: 'white' }}
+        variant="h6"
+        sx={{ p: 2, p: 1, bgcolor: green[500], color: "white" }}
       >
         Friend List
       </Typography>
@@ -49,8 +53,8 @@ const FriendList = ({ friendCount, username, friends }) => {
             component={Link}
             href={`/profile/${friend.username}`}
             to={`/profile/${friend.username}`}
-            style={{ textDecoration: 'none' }}
-            sx={{ curser: 'pointer', color: 'black' }}
+            style={{ textDecoration: "none" }}
+            sx={{ curser: "pointer", color: "black" }}
           >
             <ListItemAvatar>
               <Avatar>
@@ -60,24 +64,24 @@ const FriendList = ({ friendCount, username, friends }) => {
             <ListItemText primary={friend.username} />
           </ListItem>
         ))}
-        <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        <Box sx={{ display: "flex", flexDirection: "row-reverse", p: "1" }}>
           <Typography
             component={Link}
             // style={{ textDecoration: 'none' }}
-            to='/'
+            to="/"
             sx={{
               m: 1,
               mr: 4,
-              fontWeight: 'bold',
-              color: 'black',
-              cursor: 'pointer',
+              fontWeight: "bold",
+              color: "black",
+              cursor: "pointer",
             }}
           >
             View all
           </Typography>
         </Box>
       </List>
-    </div>
+    </Box>
   );
 };
 
--- client/src/components/PlantHistory/index.js
+++ client/src/components/PlantHistory/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import Paper from '@mui/material/Paper';
+import React from "react";
+import {
+  Grid,
+  Typography,
+  Paper,
+  Box,
+  Link,
+  ListItem,
+  ListItemText,
+  Divider,
+  List,
+} from "@mui/material";
+import { green, grey } from "@mui/material/colors";
 
 const PlantHistory = ({ history }) => {
   return (
-    <Paper className="card mb-3">
-        <div className="card-header">
-        <span className="text-light">Plant History</span>
-         </div>
-        <div className="card-body">
+    <div>
+      <Typography
+        variant="h6"
+        sx={{ p: 2, bgcolor: green[500], color: "white" }}
+      >
+        Plant History
+      </Typography>
+      <Divider />
+      <List>
         {history &&
-            history.map(history => (
-            <Paper>
-              <p className="pill mb-3" key={history._id}>
-                  {history.createdAt}:  {history.note_body}
-              </p>
-            </Paper>
-            ))}
-        </div>
-    </Paper>
+          history.map((history) => (
+            <ListItem className="plantList" style={{ textDecoration: "none" }}>
+              <ListItemText>
+                <h6>{history.createdAt}:</h6>
+                <Divider variant="inset" component="li" />
+                {history.note_body}
+              </ListItemText>
+            </ListItem>
+          ))}
+      </List>
+    </div>
+
+    // {/*
+    //     <Paper className="card mb-3">
+    //       <div className="card-header">
+    //         <span className="text-light">Plant History</span>
+    //       </div>
+    //       <div className="card-body">
+    //         {history &&
+    //           history.map((history) => (
+    //             <Paper>
+    //               <p className="pill mb-3" key={history._id}>
+    //                 {history.createdAt}: {history.note_body}
+    //               </p>
+    //             </Paper>
+    //           ))}
+    //       </div>
+    //     </Paper> */}
   );
 };
 
